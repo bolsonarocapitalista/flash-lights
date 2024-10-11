f8ede5a30c3b6f0b318ae017f289964190798f61
bolsonarocapitalista <142528137+bolsonarocapitalista@users.noreply.github.com>
2024-10-11 11:33:44 -0300

    s

---
+++ mais.js
@@ -0,0 +1,94 @@
+criaCartao(
+    'historia',
+    'Qual foi o presidente do Brasil em 1930?',
+    'Getulio Vargas')
+    
+    criaCartao(
+    'historia',
+    'Em que ano se iniciou a primeira guerra mundial?',
+    'Em 1914');
+    
+    criaCartao(
+    'Portugues',
+    'O que é realismo?',
+    'É uma escola literaria que estuda a objetividade dos fatos')
+    
+    criaCartao(
+    'historia',
+    'Qual foi o primeiro presidente do Brasil?',
+    'Marechal Deodoro da Fonseca')
+    
+    criaCartao(
+    'Geografia',
+    'O que é o Fordismo?',
+    'Modelo de produção industrial usado pelo EUA para produção de automóveis')
+    
+    criaCartao(
+    'Ingles',
+    'What means "idk" ?',
+    'I don''t know')
+    
+    criaCartao(
+    'Programação',
+    'Quem criou o Java?',
+    'James Gosling')
+    
+    criaCartao(
+    'Biotecnologia',
+    'O que é alporquia?',
+    'É uma técnica de enraizamento de um ramo ligado a planta matriz')
+    
+    criaCartao(
+    'Biotecnologia',
+    'O que é propagação vegetativa',
+    'Forma de reprodução assexuada que consiste na simples cisão de algum orgão vegetativo e posterior brotamento')
+    
+    criaCartao(
+    'Matematica',
+    'Qual é o valor de 2x + 5 = 15?',
+    'x = 5')
+    
+    criaCartao(
+    'Matematica',
+    'Qual o valor de cos240?',
+    'cos60 = -1/2')
+    
+    criaCartao(
+    'Biologia',
+    'Qual parte do corpo humano é considerado como segundo coração?',
+    'A panturrilha')
+    
+    criaCartao(
+    'Sociologia',
+    'O que é cultura?',
+    'Conjunto de conhecimentos, crenças e hábitos de um grupo social')
+    
+    criaCartao(
+    'Fisica',
+    'Qual é a unidade de medida da força no sistema internacional ?',
+    'Newton(N)'
+    
+    criaCartao(
+    'Portugues',
+    'Qual movimento literário brasileiro tem como principal autor Machado de Assis?',
+    'Realismo')
+    
+    criaCartao(
+    'Fisica',
+    'Qual a fórmula da segunda lei de Newton?',
+    'F=m.a')
+    
+    criaCartao(
+    'Biologia',
+    'Qual o nome da molécula responsável pelo transporte de oxigênio no sangue?',
+    'Hemoglobina')
+    
+    criaCartao(
+    'Biologia',
+    'Qual a principal função dos cloroplastos nas célula vegetais?',
+    'Realizar o processo de fotossintese')
+    
+    criaCartao(
+    'Matematica',
+    'Qual a fórmula para calcular a área de um circulo?',
+    'A = πr²')
