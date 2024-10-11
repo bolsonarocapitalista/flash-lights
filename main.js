3602bd44e8881f6292009893fa5a8092829b166a
bolsonarocapitalista <142528137+bolsonarocapitalista@users.noreply.github.com>
2024-10-11 11:58:57 -0300

    s

---
+++ main.js
@@ -0,0 +1,17 @@
+function criaCartao(categoria,pergunta,resposta){
+    let container =document.getElementById('container');
+    let cartao =document.createElement('article');
+    cartao.className ='cartao'
+    cartao.innerHTML =`
+    <divclass class="conteudo-cartao"> 
+    <h3>Categoria</h3>
+    <div class="pergunta-cartao">
+        <p>pertunta</p>
+
+    </div class="resposta-cartao">
+        <p>resposta</p>
+        
+    </div>
+    `
+    
+}
