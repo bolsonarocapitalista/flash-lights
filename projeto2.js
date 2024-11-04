0689bf58eada9cabf45a894a630e317c54bc1891
bolsonarocapitalista <142528137+bolsonarocapitalista@users.noreply.github.com>
2024-11-04 08:23:42 -0300

    s

--- projeto2.js
+++ projeto2.js
@@ -23,3 +23,13 @@ cartao.addEventListener('click', viraCartao);
 
     container.appendChild(cartao);
 }
+
+function mostrarResposta(card){
+    const resposta = card.querySelector('.resposta-cartao');
+    if(resposta.style.display==='none'){
+        resposta.style.display='block';
+    }else{
+        resposta.style.display='none'
+    }
+
+}
